5bc6d6acabe86eef03a6c289e435f3c823e727c2
Johannes Ebeling <ebeling@sipgate.de>
2024-07-12 11:33:02 +0200

    🎉 Initial working implementation

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-changelog-parser",
+    platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-markdown.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "1.4.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "swift-changelog-parser",
+            dependencies: [.product(name: "Markdown", package: "swift-markdown"),
+                           .product(name: "ArgumentParser", package: "swift-argument-parser")]),
+    ]
+)
---
+++ Sources/Changelog.swift
@@ -0,0 +1,64 @@
+import Foundation
+import Markdown
+import ArgumentParser
+
+@main
+struct Changelog: ParsableCommand {
+    enum Release: ExpressibleByArgument, CustomStringConvertible {
+        case unreleased
+        case release(String)
+
+        var description: String {
+            switch self {
+            case .unreleased:
+                return defaultValueDescription
+            case let .release(value):
+                return value
+            }
+        }
+
+        var defaultValueDescription: String {
+            "unreleased"
+        }
+
+        init?(argument: String) {
+            switch argument.lowercased() {
+            case "unreleased":
+                self = .unreleased
+            default:
+                self = .release(argument)
+            }
+        }
+    }
+
+    @Argument(help: "Path to Changelog.md file")
+    var path: String
+
+    @Option(name: .shortAndLong, help: "Get a specific list of changes")
+    var release: Release = .unreleased
+
+    func validate() throws {
+        guard FileManager.default.fileExists(atPath: path) else {
+            throw ValidationError("Changelog does not exist at path \(path)")
+        }
+    }
+
+    func run() throws {
+        let document = try Document(parsing: URL(filePath: path))
+        let heading = document.children
+            .compactMap { $0 as? Heading }
+            .filter { $0.level == 2 }
+            .first { $0.plainText.lowercased() == release.description }
+
+        guard let heading else {
+            throw ValidationError("Changelog does not contain '\(release.description.localizedCapitalized)' section")
+        }
+
+        let list = document.child(at: heading.indexInParent + 1)
+        guard let list, list is UnorderedList else {
+            throw ValidationError("Changelog does not contain elements in the '\(release.description.localizedCapitalized)' section")
+        }
+
+        print(list.format().trimmingCharacters(in: .whitespacesAndNewlines))
+    }
+}
